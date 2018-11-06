5b6401ab1b1747f6e1fdda0d788bfd23c1165dc1
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-11-06 07:16:17 -0300

    Create a initial version of video list

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import styled, { injectGlobal } from 'styled-components'
+import Play from 'components/play'
 
 import 'normalize.css'
 import 'milligram'
@@ -9,11 +10,20 @@ import 'milligram'
 const App = () => (
   <Container>
     <Header>
-      <h1>React Clone-963</h1>
+      <h1>React Clone</h1>
     </Header>
 
     <Main>
-      Conteúdo
+      <VideosList>
+        {Array.from({ length: 10 }).map((item, index) => (
+          <Video key={index}>
+            <VideoThumb>
+              <PlayStyled />
+            </VideoThumb>
+           <VideoTitle>Vídeo</VideoTitle>
+          </Video>
+        ))}
+      </VideosList>
     </Main>
 
     <Footer>
@@ -47,6 +57,34 @@ const Footer = styled.footer`
 
 const Container = styled.div`
   height: 100%;
+`
+
+const VideosList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const Video = styled.section`
+  flex: 1 1 300px;
+  margin: 0 5px 5px;
+`
+
+const VideoThumb = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #999;
+  height: 150px;
+`
+
+const VideoTitle = styled.h2`
+  font-size: 18px;
+`
+
+const PlayStyled = styled(Play)`
+  fill: #999;
+  height: 50px;
+  width: 50px;
 `
 
 export default App
